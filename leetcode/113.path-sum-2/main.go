b10efd2c005c2733e24a6fd21373c81e72df37f4
sol <darkbluever@gmail.com>
2020-01-02 17:19:40 +0800

    leetcode 113 v2, use internal func for recursive

--- leetcode/113.path-sum-2/main.go
+++ leetcode/113.path-sum-2/main.go
@@ -7,36 +7,24 @@ type TreeNode struct {
 }
 
 func pathSum(root *TreeNode, sum int) [][]int {
-	if root == nil {
-		return [][]int{}
-	}
-	if root.Left == nil && root.Right == nil {
-		if root.Val == sum {
-			return [][]int{[]int{root.Val}}
-		}
-		return [][]int{}
-	}
 	ret := make([][]int, 0)
-	left := pathSum(root.Left, sum-root.Val)
-	right := pathSum(root.Right, sum-root.Val)
-	if len(left) > 0 {
-		for i := range left {
-			tmp := make([]int, 0, len(left[i])+1)
-			tmp = append(tmp, root.Val)
-			tmp = append(tmp, left[i]...)
-			left[i] = tmp
-		}
-		ret = append(ret, left...)
-	}
-	if len(right) > 0 {
-		for i := range right {
-			tmp := make([]int, 0, len(right[i])+1)
-			tmp = append(tmp, root.Val)
-			tmp = append(tmp, right[i]...)
-			right[i] = tmp
-		}
-		ret = append(ret, right...)
-	}
+	path := make([]int, 0)
+	singlePathSum(root, sum, path, &ret)
 
 	return ret
+}
+
+func singlePathSum(node *TreeNode, sum int, path []int, result *[][]int) {
+	if node == nil {
+		return
+	}
+	path = append(path, node.Val)
+	if node.Left == nil && node.Right == nil && node.Val == sum {
+		ret := make([]int, len(path))
+		copy(ret, path)
+		*result = append(*result, ret)
+		return
+	}
+	singlePathSum(node.Left, sum-node.Val, path, result)
+	singlePathSum(node.Right, sum-node.Val, path, result)
 }
