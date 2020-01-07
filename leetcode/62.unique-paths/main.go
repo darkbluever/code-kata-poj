dc88aad38c296acd02fd37f422607a326240fe61
sol <darkbluever@gmail.com>
2020-01-07 20:18:58 +0800

    leecode 62 v4, dp v3

--- leetcode/62.unique-paths/main.go
+++ leetcode/62.unique-paths/main.go
@@ -1,17 +1,14 @@
 package main
 
 func uniquePaths(m int, n int) int {
-	pre := make([]int, n)
-	for j := 0; j < n; j++ {
-		pre[j] = 1
-	}
 	cur := make([]int, n)
-	copy(cur, pre)
+	for j := 0; j < n; j++ {
+		cur[j] = 1
+	}
 	for i := 1; i < m; i++ {
 		for j := 1; j < n; j++ {
-			cur[j] = pre[j] + cur[j-1]
+			cur[j] += cur[j-1]
 		}
-		pre = cur
 	}
 	return cur[n-1]
 }
