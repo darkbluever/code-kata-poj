fbf79a9c09b3c8d07051d93fa1c82abd3be3e12c
sol <darkbluever@gmail.com>
2020-01-08 20:06:11 +0800

    leetcode 337 v3 dp

--- leetcode/337.house-robber-3/main.go
+++ leetcode/337.house-robber-3/main.go
@@ -1,51 +1,105 @@
 package main
 
+/**
+ * define of optimal substructure
+ *          root
+ *         /    \
+ *        l      r
+ *       / \    / \
+ *      ll  lr rl  rr
+ * max(root) = max(root+ll+lr+rl+rr, l+r, l+rl+rr, r+ll+lr)
+ * =>  max(root) = max(root+max(ll)+max(lr)+max(rl)+max(rr), max(l)+max(r))
+ */
+
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
 	Right *TreeNode
 }
 
-func rob(root *TreeNode) int {
-	cache := make(map[string]int)
-	return singleRob(root, false, &cache)
+type SimpleStack struct {
+	Nodes []*TreeNode
 }
 
-func singleRob(node *TreeNode, skip bool, cache *map[string]int) int {
-	if node == nil {
+func NewStack() *SimpleStack {
+	return &SimpleStack{[]*TreeNode{}}
+}
+
+func (s *SimpleStack) Push(node *TreeNode) {
+	s.Nodes = append(s.Nodes, node)
+}
+
+func (s *SimpleStack) Pop() *TreeNode {
+	n := len(s.Nodes)
+	if n > 0 {
+		node := s.Nodes[n-1]
+		s.Nodes = s.Nodes[0 : n-1]
+		return node
+	}
+	return nil
+}
+
+func (s *SimpleStack) Peek() *TreeNode {
+	n := len(s.Nodes)
+	if n > 0 {
+		node := s.Nodes[n-1]
+		return node
+	}
+	return nil
+}
+
+func (s *SimpleStack) Empty() bool {
+	return len(s.Nodes) == 0
+}
+
+func rob(root *TreeNode) int {
+	if root == nil {
 		return 0
 	}
+	dp := make(map[*TreeNode]int)
+	stack := NewStack()
+	stack.Push(root)
 
-	chooseLeftKey := fmt.Sprintf("%p-choose", node.Left)
-	chooseLeft, ok := (*cache)[chooseLeftKey]; if !ok {
-		chooseLeft = singleRob(node.Left, false, cache)
-		(*cache)[chooseLeftKey] = chooseLeft
+	var cur = root
+	var pre *TreeNode
+	for !stack.Empty() {
+		// postorder travelsal
+		for cur != nil {
+			stack.Push(cur)
+			cur = cur.Left
+		}
+		cur = stack.Peek()
+		if cur.Right == nil || cur.Right == pre {
+			stack.Pop()
+			dp[cur] = max(robNode(cur, dp), dp[cur.Left] + dp[cur.Right])
+			pre = cur
+			cur = nil
+		} else {
+			cur = cur.Right
+		}
 	}
+	return dp[root]
+}
 
-	chooseRightKey := fmt.Sprintf("%p-choose", node.Right)
-	chooseRight, ok := (*cache)[chooseRightKey]; if !ok {
-		chooseRight = singleRob(node.Right, false, cache)
-		(*cache)[chooseRightKey] = chooseRight
+func robNode(node *TreeNode, dp map[*TreeNode]int) int {
+	sum := node.Val
+	if node.Left != nil {
+		if node.Left.Left != nil {
+			sum += dp[node.Left.Left]
+		}
+		if node.Left.Right != nil {
+			sum += dp[node.Left.Right]
+		}
 	}
-	skipThis := chooseLeft + chooseRight
-	if skip {
-		return skipThis
+	if node.Right != nil {
+		if node.Right.Left != nil {
+			sum += dp[node.Right.Left]
+		}
+		if node.Right.Right != nil {
+			sum += dp[node.Right.Right]
+		}
 	}
-
-	skipLeftKey := fmt.Sprintf("%p-skip", node.Left)
-	skipLeft, ok := (*cache)[skipLeftKey]; if !ok {
-		skipLeft = singleRob(node.Left, true, cache)
-		(*cache)[skipLeftKey] = skipLeft
-	}
-
-	skipRightKey := fmt.Sprintf("%p-skip", node.Right)
-	skipRight, ok := (*cache)[skipRightKey]; if !ok {
-		skipRight = singleRob(node.Right, true, cache)
-		(*cache)[skipRightKey] = skipRight
-	}
-	chooseThis := node.Val + skipLeft + skipRight
-
-	return max(chooseThis, skipThis)
+	return sum
 }
 
 func max(a, b int) int {
