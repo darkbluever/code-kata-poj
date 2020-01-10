0c5f33fdda5e5f8de90d1f138f35a8b2e5aa4b28
sol <darkbluever@gmail.com>
2020-01-10 12:05:45 +0800

    leetcode 139 v2, dp

--- leetcode/139.word-break/main.go
+++ leetcode/139.word-break/main.go
@@ -1,73 +1,20 @@
 package main
 
-var a = rune('a')
-
-type TreeNode struct {
-	count    int
-	children [26]*TreeNode
-}
-
-func NewTireTree() *TreeNode {
-	return &TreeNode{}
-}
-
-func (t *TreeNode) Add(s string) {
-	if len(s) == 0 {
-		return
-	}
-	r := []rune(s)
-	cur := t
-	for i := range r {
-		idx := r[i] - a
-		if cur.children[idx] == nil {
-			cur.children[idx] = NewTireTree()
-		}
-		cur = cur.children[idx]
-	}
-	cur.count = 1
-}
-
-func (t *TreeNode) Get(s string) int {
-	if len(s) == 0 {
-		return 1
-	}
-	r := []rune(s)
-	cur := t
-	for i := range r {
-		idx := r[i] - a
-		if cur.children[idx] == nil {
-			return -1
-		}
-		cur = cur.children[idx]
-	}
-	return cur.count
-}
-
 func wordBreak(s string, wordDict []string) bool {
-	if len(wordDict) == 0 || len(s) == 0 {
-		return false
+	dp := make([]bool, len(s) + 1)
+	dp[0] = true
+	wordMap := make(map[string]struct{}, len(wordDict))
+	for k := range wordDict {
+		wordMap[wordDict[k]] = struct{}{}
 	}
-	root := NewTireTree()
-	for i := range wordDict {
-		root.Add(wordDict[i])
-	}
-	return walk(s, root)
-}
 
-func walk(s string, t *TreeNode) bool {
-	flag := false
-	for i := range s {
-		code := t.Get(s[0:i+1])
-		if code == -1 {
-			return flag
-		}
-		if code == 1 {
-			if i == len(s) - 1 {
-				return true
+	for i := 0; i < len(s); i++ {
+		for j := i + 1; j <= len(s); j++ {
+			if _, ok := wordMap[s[i:j]]; dp[i] && ok {
+				dp[j] = true
 			}
-			b := walk(s[i+1:], t)
-			flag = flag || b
 		}
 	}
-	return flag
+
+	return dp[len(s)]
 }
